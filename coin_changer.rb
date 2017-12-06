3f9bafcff208758d110776773a62454d1e5a1866
ftmanspile <codfan15@gmail.com>
2017-12-06 11:34:58 -0500

    initial start of function

---
+++ coin_changer.rb
@@ -0,0 +1,9 @@
+def get_change(cents_recieved)
+	coins_returned = {}
+	coins = ("quarter" => 25, "dime" => 10, "nickel" => 5, "penny" => 1)
+	
+	puts "Amount given is #{cents_received}"
+	amount_given = cents_received
+end
+
+get_change(50)
